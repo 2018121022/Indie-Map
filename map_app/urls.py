aabbcc7825e29b46d697f2acbd9a8c808875347c
2018124058 <2018124058@yonsei.ac.kr>
2022-05-29 17:17:08 +0900

    [map] 로그인 전후 홈화면 지도, 개별 공연 지도

--- map_app/urls.py
+++ map_app/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', map, name="map"),
+    path('login/', login_map, name="login_map"),
+    path('each/', each_map, name="each_map" )
 ]
--- map_app/views.py
+++ map_app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def map(request):
     return render(request, 'map.html')
+
+def login_map(request):
+    return render(request, 'login_map.html')
+
+def each_map(request):
+    return render(request, 'each_map.html')
