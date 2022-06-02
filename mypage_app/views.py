896cef5ca654860105894660d153ee239a1e148d
2018124058 <2018124058@yonsei.ac.kr>
2022-06-02 23:39:04 +0900

    map

--- mypage_app/views.py
+++ mypage_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from account_app.models import User
+from indie_app.models import Concert
 
 # Create your views here.
 
@@ -28,3 +30,19 @@ def musicpage_2(request):
 def musicpage_1(request):
     return render(request, 'musicpage_1.html')
 
+# 공연 등록 창
+def concert_form(request):
+    return render(request, 'concert_form.html')
+
+# 공연 등록 
+def concert_create(request):
+    if(request.method == 'POST'):
+        concert = Concert() 
+        concert.musician = request.user
+        concert.introduce = request.POST['introduce'] 
+        concert.date = request.POST['date']
+        concert.time = request.POST['time']
+        concert.latitude = request.POST['latitude']
+        concert.longitude = request.POST['longitude']
+        concert.save() 
+    return redirect('home') 
