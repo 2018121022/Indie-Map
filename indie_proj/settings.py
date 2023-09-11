72e72fefb2b1e70d6f79590929276e2cc47520e2
Jinnmidnnight <vkstk0131@yonsei.ac.kr>
2023-09-11 16:25:06 +0900

    allowed host

--- indie_proj/settings.py
+++ indie_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^^73!z#35(#_vkqef(sq$ala(6jibe28+s^o%*595-mc%a64ge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['indiemap.pythonanywhere.com/', '127.0.0.1' ]
+ALLOWED_HOSTS = ['indiemap.pythonanywhere.com', '127.0.0.1' ]
 
 AUTH_USER_MODEL = 'account_app.User'
 AUTH_FOLLOWER_MODEL = "account_app.User"
